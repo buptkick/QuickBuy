5a3d4a7e05cf44882d9175e086fd5702bc0619bc
buptkick <buptkick@gmail.com>
2020-01-06 10:30:26 +0800

    Update count-worker.js

--- count-worker.js
+++ count-worker.js
@@ -1,14 +1,22 @@
 let self = this;
 
 function timer(time) {
-    if (time < 0) {
+    if (time <= 0) {
         self.postMessage(-1);
         return self.close();
     }
-    self.postMessage(time);
-    setTimeout(() => {
-        timer(time - 1000);
-    }, 1000)
+    else if (time < 1000) {
+        self.postMessage(time);
+        setTimeout(() => {
+            timer(time - 10);
+        }, 10)
+    }
+    else {
+        self.postMessage(time);
+        setTimeout(() => {
+            timer(time - 1000);
+        }, 1000)
+    }
 }
 
 self.onmessage = function (event) {
